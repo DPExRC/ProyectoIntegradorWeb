478b475f34abb3610f7927acbefea4d96103bbea
DPExRC <davidpierexequiel.raimilla@alumnos.ulagos.cl>
2023-10-03 19:55:04 -0300

    se suben cambios en las grillas e interfaz de volteo, contador y temporizador

--- pr1.js
+++ pr1.js
@@ -1,9 +1,19 @@
-const letters = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+let letters = [];
 let flippedCards = [];
 let matchedCards = [];
 let moves = 0;
 let timer = 0;
+let score = 0;
 let timerInterval;
+let numRows = 4;
+let numCols = 4;
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        timer++;
+        document.getElementById('timer').textContent = `Tiempo: ${timer} segundos`; 
+    }, 1000);
+}
 
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
@@ -23,35 +33,25 @@ function shuffle(array) {
 function createCard(value) {
     const card = document.createElement('div');
     card.classList.add('card');
-    
-    // Agregar imagen
     const image = document.createElement('img');
-    image.src = '../Proyecto/images.jpeg'; // Ruta de la imagen
-    image.style.width = '100px'; // Establecer un ancho fijo
-    image.style.height = '100px'; // Establecer una altura fija
+    image.src = '../Proyecto/images.jpeg'; 
+    image.style.width = '100px'; 
+    image.style.height = '100px'; 
     card.appendChild(image);
-
-    // Agregar evento de clic
     card.addEventListener('click', () => flipCard(card));
-
-    // Agregar letra como data-value
     card.dataset.value = value;
-
     return card;
 }
 
 function flipCard(card) {
     if (flippedCards.length < 2 && !flippedCards.includes(card)) {
-        // Mostrar letra al clickear
         card.innerHTML = card.dataset.value;
         flippedCards.push(card);
-
         if (flippedCards.length === 2) {
             setTimeout(checkMatch, 1000);
         }
     }
 }
-
 
 function updateSelectedCards() {
     const selectedCardsElement = document.getElementById('selectedCards');
@@ -64,42 +64,90 @@ function checkMatch() {
 
     if (card1.textContent === card2.textContent) {
         setTimeout(() => {
-            card1.remove(); // Elimina la carta del DOM
-            card2.remove(); // Elimina la carta del DOM
+            card1.remove(); 
+            card2.remove(); 
         }, 1000);
 
         matchedCards.push(card1, card2);
         flippedCards = [];
 
+        score += 10
+        document.getElementById('score').textContent = `Puntaje: ${score}`;
+
         if (matchedCards.length === letters.length) {
             clearInterval(timerInterval);
             alert(`¡Has ganado! Movimientos: ${moves} - Tiempo: ${timer} segundos`);
         }
+
     } else {
         flippedCards.forEach(card => {
-            card.textContent = ''; // Voltea las cartas de nuevo
+            card.textContent = ''; 
         });
         flippedCards = [];
     }
 }
 
-
-
-
-
-
 function initializeGame() {
     const shuffledLetters = shuffle(letters);
     const memoryBoard = document.getElementById('memoryBoard');
-
     shuffledLetters.forEach((value, index) => {
         const card = createCard(value);
         card.dataset.value = value;
         memoryBoard.appendChild(card);
     });
-
     startTimer();
+    document.getElementById('timer').textContent = `Tiempo: ${timer} segundos`; 
+    document.getElementById('score').textContent = `Puntaje: ${score}`;
+}
+
+function resetGame() {
+    clearInterval(timerInterval);
+    flippedCards = [];
+    matchedCards = [];
+    moves = 0;
+    timer = 0;
+    score = 0;
+    initializeGame();
+}
+
+function generateNumbers(n) {
+    let numbers = [];
+
+    for (let i = 1; i <= n; i++) {
+        numbers.push(i.toString(), i.toString());
+    }
+
+    return numbers;
+}
+
+function setDifficulty(level) {
+    let n;
+    switch (level) {
+        case 'easy':
+            n = 8;
+            numRows = 4;
+            numCols = 4;
+            break;
+        case 'medium':
+            n = 18;
+            numRows = 6;
+            numCols = 6;
+            break;
+        case 'hard':
+            n = 32;
+            numRows = 8;
+            numCols = 8;
+            break;
+    }
+
+    letters = generateNumbers(n);
+
+    const memoryBoard = document.getElementById('memoryBoard');
+    memoryBoard.innerHTML = ''; 
+    memoryBoard.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
+    memoryBoard.style.gridTemplateColumns = `repeat(${numCols}, 1fr)`;
+    initializeGame();
 }
 
 
-initializeGame();
+setDifficulty('easy');
